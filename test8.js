62f2bd2afdbe8a3c60a51d950de20320e0b45c0c
aodlichao2012-lnw <aodlichao2012@hotmail.com>
2019-07-25 10:02:28 +0700

    55555

---
+++ test8.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+app.set('view engine','jade');
+app.set('view','.src/..');
+
+app.get('/',function(req,res)
+{
+    res.render('index');
+});
+
+app.listen(PORT ,function(err)
+{
+    if(!err)
+    {
+        console.log('Server is running at PORT', PORT);
+    }
+    else
+    {
+        console.log(JSON.stringify(err));
+    }
+});
